4988c8076118e71613f31882516a983e3cecdd3d
Michel Serrano <maserranomonzon@gmail.com>
2018-05-27 12:44:42 +0200

    Servicio Firebase Agregado

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,15 +7,31 @@ export class InformacionService {
   info:any = {};
   cargada:boolean = false;
 
+  info_about:any[] = [];
+  cargada_about:boolean = false;
+
   constructor(public http:Http) { 
 
-    this.http.get("assets/data/info.pagina.json")
-            .subscribe(data =>{
-              this.info = data.json();
-              this.cargada = true;
-              console.log(data)
-            })
+    this.carga_info();
+    this.carga_about();
 
+  }
+
+  public carga_info(){
+    this.http.get("assets/data/info.pagina.json")
+              .subscribe(data =>{
+                this.info = data.json();
+                this.cargada = true;
+              })
+  }
+
+  public carga_about(){
+    this.http.get("https://webangular-4f410.firebaseio.com/equipo.json")
+              .subscribe(data =>{
+                this.info_about = data.json();
+                this.cargada_about = true;
+                console.log(data)
+              })
   }
 
 }
