88e433c196643423d09c4876a6f0b264edc66a21
Michel Serrano <maserranomonzon@gmail.com>
2018-05-27 20:01:31 +0200

    Search finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProductoComponent } from './components/producto/producto.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
     { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'producto/:id', component: ProductoComponent },
+    { path: 'buscar/:termino', component: SearchComponent },
     { path: '**', pathMatch:'full', redirectTo: 'home' }
 ];
 
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent {
+
+termino:string = undefined;
+
+  constructor(private route:ActivatedRoute,
+              private _ps:ProductosService) { 
+
+    route.params.subscribe(res =>{
+      this.termino = res['termino'];
+      console.log(this.termino);
+
+      _ps.buscar_productos(this.termino);
+    })
+  }
+
+
+
+}
